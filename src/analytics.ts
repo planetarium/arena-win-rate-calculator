da5162680acef55b48a48727225bbf8dfe930120
Atralupus <me@atralupus.com>
2024-02-14 10:58:39 +0900

    Apply ga

---
+++ src/analytics.ts
@@ -0,0 +1,8 @@
+import ReactGA from "react-ga4";
+
+const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
+
+export const initGA = () => {
+    ReactGA.initialize(GA_MEASUREMENT_ID);
+};
+
